5540774c4610dc68aabf6a7fa0b19a7cd0767a1b
akil4n <akilanmaruthachalam5678@gmail.com>
2022-02-17 21:00:34 +0530

    print all possible substrings [permutation with spaces] ✔️

--- src/io/akil4n/Recursion/PrintAllPossibleSubstrings.java
+++ src/io/akil4n/Recursion/PrintAllPossibleSubstrings.java
@@ -1,8 +1,9 @@
 package io.akil4n.Recursion;
 
 import java.util.ArrayList;
+import java.util.List;
 
-/*
+/**
 TODO:
 Given a string str your task is to complete the function spaceString which takes only one argument the string str and  finds all possible strings that can be made by placing spaces (zero or one) in between them.
 
@@ -25,18 +26,34 @@ Example 1:
 Input:
 str = abc
 Output: abc$ab c$a bc$a b c$
-Example 2:
 
-Input:
-str = xy
-Output: xy$x y$
+ <h1>RECURSIVE TREE<h1/>
+
+                                 bcd
+                             /        \
+                          bcd           bc_d
+                        /   \          /    \
+                     bcd    b_cd   bc_d   b_c_d
+
 */
 public class PrintAllPossibleSubstrings {
-    ArrayList<String> spaceString(String str, String substr, int index, ArrayList<String> ans) {
-        return null;
+    static List<String> spaceString(String source, List<String> results, int index) {
+        if (index == 0) {
+            results.add(source.trim());
+            return results;
+        }
+        spaceString(source, results, index - 1);
+        StringBuilder spaceString = new StringBuilder(source).insert(index, " ");
+        spaceString(new String(spaceString), results, index - 1);
+        return results;
     }
 
     public static void main(String[] args) {
-
+        List<String> res = new ArrayList<>();
+        String source = "abc";
+        spaceString(source, res, source.length() - 1);
+        for (String e : res) {
+            System.out.print(e+"$");
+        }
     }
 }
